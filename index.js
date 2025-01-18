30f6adbe28c1fbb4a3bac7ddb5e8a5e4c95e0742
NureAlam68 <nurealam151068@gmail.com>
2025-01-19 00:05:37 +0600

    verifyToken with jwt

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -24,6 +25,42 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const userCollection = client.db("TechHiveDB").collection("users");
+
+
+    // jwt related api
+    app.post('/jwt', async(req, res) => {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+          expiresIn: '30d'
+        });
+        res.send({token})
+      })
+
+      // middlewares
+    const verifyToken = (req, res, next) => {
+        if(!req.headers.authorization) {
+          return res.status(401).send({message: 'unauthorized access'})
+        }
+        const token = req.headers.authorization.split(' ')[1];
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+          if(err){
+            return res.status(401).send({message: 'unauthorized access'})
+          }
+          req.decoded = decoded;
+          next(); 
+        })
+      }
+
+      // user related api
+    app.get('/users', verifyToken, async(req, res) => {
+      // console.log(req.headers)
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");

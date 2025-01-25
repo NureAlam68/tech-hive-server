892e969368780d18f56a96f730ea422203a553ab
NureAlam68 <nurealam151068@gmail.com>
2025-01-25 11:30:39 +0600

    payment successfull

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -71,6 +72,12 @@ async function run() {
       // console.log(req.headers)
       const result = await userCollection.find().toArray();
       res.send(result);
+    });
+
+    app.get('/users/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email });
+      res.send(user);
     });
 
     app.get("/users/admin/:email", verifyToken, async (req, res) => {
@@ -441,6 +448,34 @@ app.delete("/reported-products/:id", verifyToken, async (req, res) => {
         res.status(500).send({ message: "Internal Server Error" });
       }
     });
+
+    // payment intent
+    app.post('/create-payment-intent', async(req, res) => {
+      const { amount } = req.body;
+      const amountInt = parseInt(amount * 100);
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amountInt,
+        currency: 'usd',
+        payment_method_types: ['card']
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
+    })
+
+    // Update user's subscription status after payment
+app.post('/users/subscribe', async (req, res) => {
+  const { email, transactionId } = req.body;
+
+  const filter = { email };
+  const updateDoc = {
+    $set: { isSubscribed: true, transactionId },
+  };
+
+  const result = await userCollection.updateOne(filter, updateDoc);
+  res.send(result);
+});
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
